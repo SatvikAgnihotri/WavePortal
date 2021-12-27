94637cb5b6047479f853d396015e3b838775e648
SatvikAgnihotri <satvikagnihotri12@gmail.com>
2021-12-27 10:59:49 -0500

    Update hardhat.config.js

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -1,17 +1,3 @@
-require('@nomiclabs/hardhat-waffle');
-
-module.exports = {
-  solidity: '0.8.0',
-  networks: {
-    rinkeby: {
-      //DO NOT Commit the following to Github//
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/1u7UBdT_XaD8jr4OJLi-VupO7U20KA-i', 
-      accounts: ['4e6d4d4b06c9d083ad52a010164387d0243c6af23745e5f9adba6b11e12d2d8e'],
-    },
-  },
-};
-
-
 //require('@nomiclabs/hardhat-waffle');
 
 //module.exports = {
